64c1a0939d7883aba6efbdd2b07488ce93747035
Kalashnikov Max <2957851alwyd@gmail.com>
2018-08-15 16:46:02 +0000

    Fixed warnings

--- Source/Buttons/Button.cpp
+++ Source/Buttons/Button.cpp
@@ -1,8 +1,8 @@
 #include "Button.hpp"
 
 Button::Button(sf::Vector2f &pos, std::string &message, sf::Font &font, std::function<int()> function)
-: function(function)
-, relativePosition(pos)
+: relativePosition(pos)
+, function(function)
 {
     text.setFont(font);
     text.setFillColor(sf::Color::Black);
